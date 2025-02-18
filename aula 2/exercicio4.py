567a70da8595f9d8d02766d00dddebeb38fba8fc
souzabee <arturgsouza@outlook.com>
2025-02-17 22:13:50 -0300

    exercicio4

--- aula 2/exercicio4.py
+++ aula 2/exercicio4.py
@@ -5,8 +5,8 @@ IR = salario_bruto * 0.11
 INSS = salario_bruto * 0.08
 sindicato = salario_bruto * 0.05
 salario_liquido = salario_bruto - IR - INSS - sindicato
-print(f"+ Salario Bruto: {salario_bruto:.2f}  - INSS {INSS:.2f} - Sindicato {sindicato:.2f} + {salario_liquido:.2f}")
-print(f"- IR (11%) {IR:.2f}")
-print(f"- INSS (8%) {INSS:.2f}")
-print(f"- Sindicato (5%) {sindicato:.2f}")
-print(f"+ {salario_liquido:.2f}")
+print(f"+ Salario Bruto: {salario_bruto:.2f}")
+print(f"- IR (11%): R$ {IR:.2f}")
+print(f"- INSS (8%): R$ {INSS:.2f}")
+print(f"- Sindicato (5%): R$ {sindicato:.2f}")
+print(f"+ R$ {salario_liquido:.2f}")
