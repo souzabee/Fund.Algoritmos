1fdbe28e49caa3213681a41485e1448c213dc060
souzabee <arturgsouza@outlook.com>
2025-02-17 21:24:00 -0300

    exercicio2

--- aula 2/exercicio2.py
+++ aula 2/exercicio2.py
@@ -1,4 +1,4 @@
 km_totais = float(input("digite a quantidade de km percorridos: "))
 dias = int(input("digite a quantidade de dias alocados: "))
 valor_total = (60 * dias) + (km_totais * 0.15)
-print("Total a pagar:", valor_total)
+print(f"Total a pagar: {valor_total:.2f}" )
